5ef77d4dd72a00e1abe2781469e54bc023e45e89
Karn Ashutosh Singh <karns7844@gmail.com>
2025-09-15 23:21:36 +0530

    folder structure

---
+++ index.js
@@ -0,0 +1,13 @@
+import dotenv from "dotenv";
+
+
+dotenv.config({
+    path: " ./.env"
+})
+
+let myusername=process.env.dataZ;
+
+console.log("value",myusername);
+
+console.log("start of backend project ");
+console.log("start of the backend project ");
